842c454aeeaca43c6eee8cbb2a2406a0fd6d958b
matt <filmarvel@163.com>
2020-08-21 13:56:03 +0800

    1.2

--- week01/task1/controller.py
+++ week01/task1/controller.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
+#获得前10个电影链接列表
 def moviesurl(myurl):
     user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
 
@@ -17,6 +18,8 @@ def moviesurl(myurl):
             urls.append(f'https://maoyan.com' + atag.get('href'))
     return urls
 
+
+#获得全部电影信息
 def moviesinfo(urls):
     mylist = []
     for url in urls:
@@ -28,14 +31,16 @@ def moviesinfo(urls):
 
         bs_info = bs(response.text, 'html.parser')
 
-        #获取电影名称、类型、上映时间
+        #获取电影名称
         movie_name = bs_info.find('h1', attrs={'class':'name'}).text
 
+        #类型
         movie_cate_list = bs_info.find_all('a', attrs={'class':'text-link'})
         movie_cates = ''
         for movie_cate in movie_cate_list:
             movie_cates += movie_cate.text
 
+        #上映时间
         movie_release_date = bs_info.find_all('li', attrs={'class':'ellipsis'})[2].text[:10]
         
         info_list= [movie_name, movie_cates, movie_release_date]
